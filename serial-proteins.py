40ef00579fa8c20761c4539e59f2e8345655800b
leiredsol09 <ldominguezsolusa@gmail.com>
2024-10-14 12:40:27 +0200

    Update serial-proteins.py

--- serial-proteins.py
+++ serial-proteins.py
@@ -9,6 +9,7 @@ start_time = time()
 
 mydata= pd.read_csv('proteins.csv', delimiter= ',')
 mydata['contains_pattern']= mydata['sequence'].str.contains(pattern)
+
 ids_true = mydata[mydata['contains_pattern']==True].copy()
 ids_true.loc[:, 'repetitions_pattern']= ids_true['sequence'].str.count(pattern)
 ids_true = ids_true.sort_values(by = ['repetitions_pattern'], ascending=False)
@@ -19,11 +20,14 @@ end_time = time()
 ids_true = ids_true.head(10)
 
 print('Execution time: ', end_time-start_time, 'seconds')
-print('Maximum protein: ', ids_true.iloc[0, 0], ' and occurrences: ', ids_true.iloc[0,-1])
+if ids_true.empty: 
+    print('Pattern not found')
+else: 
+    print('Maximum protein: ', ids_true.iloc[0, 0], ' and occurrences: ', ids_true.iloc[0,-1])
 
-plt.bar(ids_true['structureId'], ids_true['repetitions_pattern'])
-plt.xlabel("Proteins's ID")
-plt.ylabel("Occurrences")
-plt.title('Bar plot of Id vs Number of repeated patterns')
-plt.tight_layout()
-plt.show()
+    plt.bar(ids_true['structureId'], ids_true['repetitions_pattern'])
+    plt.xlabel("Proteins's ID")
+    plt.ylabel("Occurrences")
+    plt.title('Bar plot of Id vs Number of repeated patterns')
+    plt.tight_layout()
+    plt.show()
